580dcb6770ccc54053b6b61eb369fea904868b4a
Akarshan Nirwan <141826161+Zxenith@users.noreply.github.com>
2024-07-07 20:20:24 +0530

    Updated concurrency Data_Retrieval.py

--- Data_Retrieval.py
+++ Data_Retrieval.py
@@ -19,26 +19,59 @@ def station_text(tr):
     station_name_distance = tr.find('td', class_= 'left')
     station_name = station_name_distance.find('a').text.strip()
     return station_name
-def scrape_table(part, URL):
+
+
+def scrape_table(part):
     try:
         response = requests.get(URL)
         print(response)
         logging.info(f'Scraping {part} table')
         soup = BeautifulSoup(response.text, 'html.parser')
         parting_section = soup.find('section', attrs={'id': f'{part}'})
+        logging.info(f'Scraping {part} table section')
 
         parting_table = parting_section.find('table')
         parting_body = parting_table.find_all('tr')
         station_names = []
+        logging.info(f'Scraping {part} table name')
 
         for i in range(1, len(parting_body)):
             station_names.append(station_text(parting_body[i]))
+
+        logging.info("Names generated")
 
         return station_names
 
     except Exception as e:
         logging.error(f'Error fetching {e}')
 
+def convert_to_json(line):
+    try:
+        logging.info(f'Converting {line}')
+        lines = scrape_table(line)
+        convert = {line : lines}
 
-scrape_table('RedLine', URL)
+        with open(f'{line}.json', 'w') as outfile:
+            json.dump(convert, outfile)
+
+        logging.info(f'Finished converting {line}')
+
+    except Exception as e:
+        logging.error(f'Error converting: {e}')
+
+def convert_all(stations_lines):
+    try:
+        logging.info(f'Converting all lines')
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            executor.map(convert_to_json, stations_lines)
+        logging.info(f'Concurrency achieved.')
+
+    except Exception as e:
+        logging.error(f'Error converting: {e}')
+
+
+
+if __name__ == '__main__':
+    convert_all(['RedLine', 'PinkLine', 'VioletLine', 'YellowLine', 'GreenLine', 'BlueLine', 'GrayLine'])
 
